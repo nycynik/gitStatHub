45b7173188a78b17af687bdd7db3f59453e7cc4a
Marco Alagna <malagna@amplify.com>
2017-10-12 16:20:06 -0400

    Add githup api provider

--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -1,26 +1,28 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { Headers } from '@angular/http';
 import { Injectable } from '@angular/core';
 
 /**
  * Api is a generic REST Api handler. Set your API url first.
  */
-@Injectable()
+ @Injectable()
 export class Api {
   url: string = 'https://example.com/api/v1';
 
   constructor(public http: HttpClient) {
   }
 
-  get(endpoint: string, params?: any, reqOpts?: any) {
-    if (!reqOpts) {
-      reqOpts = {
-        params: new HttpParams()
-      };
-    }
+  createHeaders() {
+    let headers = new Headers();
+    return headers;
+  }
+
+  get(endpoint: string, params?: any, reqOpts = {}) {
+    reqOpts.params = new HttpParams();
+    reqOpts.headers = this.createHeaders();
 
     // Support easy query params for GET requests
     if (params) {
-      reqOpts.params = new HttpParams();
       for (let k in params) {
         reqOpts.params.set(k, params[k]);
       }
@@ -29,19 +31,23 @@ export class Api {
     return this.http.get(this.url + '/' + endpoint, reqOpts);
   }
 
-  post(endpoint: string, body: any, reqOpts?: any) {
+  post(endpoint: string, body: any, reqOpts = {}) {
+    reqOpts.headers = this.createHeaders();
     return this.http.post(this.url + '/' + endpoint, body, reqOpts);
   }
 
-  put(endpoint: string, body: any, reqOpts?: any) {
+  put(endpoint: string, body: any, reqOpts = {}) {
+    reqOpts.headers = this.createHeaders();
     return this.http.put(this.url + '/' + endpoint, body, reqOpts);
   }
 
-  delete(endpoint: string, reqOpts?: any) {
+  delete(endpoint: string, reqOpts = {}) {
+    reqOpts.headers = this.createHeaders();
     return this.http.delete(this.url + '/' + endpoint, reqOpts);
   }
 
-  patch(endpoint: string, body: any, reqOpts?: any) {
+  patch(endpoint: string, body: any, reqOpts = {}) {
+    reqOpts.headers = this.createHeaders();
     return this.http.put(this.url + '/' + endpoint, body, reqOpts);
   }
 }
---
+++ src/providers/api/github-api.ts
@@ -0,0 +1,23 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Headers } from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Api } from 'api';
+
+@Injectable()
+export class GithubApi extends Api {
+    url: string = 'https://api.github.com';
+
+    constructor() {
+      super.constructor();
+    }
+
+    createHeaders() {
+      let headers = super.createHeaders();
+      headers.append('content-type', 'application/vnd.github.v3+json');
+      return headers;
+    }
+
+    getUserRepos(userName: string) {
+      return this.get('users/${userName}/repos');
+    }
+}
